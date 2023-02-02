82af1751be151a9069a3ff5947ab311ddf58201a
ARTHUR DE MATOS ELSNER <arthurdev93@gmail.com>
2023-02-01 22:10:26 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
 import Link from "../src/components/link";
+import React from "react";
+
+function Title ( {Children} ) {
+    return (
+        <React.Fragment>
+            <h1>
+                {Children}
+            </h1>
+            <style jsx>{`
+                h1 {
+                    color: red;
+                }
+            `}</style>
+        </React.Fragment>
+    );
+}
 
 export default function HomePage () {
     return (
         <div>
-            <h1>Alura Cases - Home</h1>
+            <Title as="h1">Alura Cases - Home</Title>
             <Link href="/faq">
                 Ir para a página do FAQ
             </Link>
