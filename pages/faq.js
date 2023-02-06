0b90cd5fc0081191f3be933b3997bc876457ac1f
ARTHUR DE MATOS ELSNER <arthurdev93@gmail.com>
2023-02-06 17:20:42 -0300

    Update faq.js

--- pages/faq.js
+++ pages/faq.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import Link from "../src/components/Link";
 
+//em modo dev ('yarn dev') sempre roda a cada acesso, como se comporta o getServerSideProps()
 export async function getStaticProps() {
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
     const faq = await fetch(FAQ_API_URL)
