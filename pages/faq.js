c65b7d4a670f6aeaf1a759014e13c7977da1c69e
ARTHUR DE MATOS ELSNER <arthurdev93@gmail.com>
2023-02-06 11:14:23 -0300

    Update faq.js

--- pages/faq.js
+++ pages/faq.js
@@ -1,11 +1,38 @@
+import { useEffect, useState } from "react";
 import Link from "../src/components/Link";
 
 
+
+
 export default function FAQPage () {
+    const [faq, setFaq] = useState([]);
+    useEffect(() => {
+        const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+        fetch(FAQ_API_URL)
+            .then((respostaDoServidor) => {
+                return respostaDoServidor.json();
+            })
+            .then((resposta) => {
+                setTimeout(() => {
+                    setFaq(resposta);
+                }, 3 * 1000);
+            });
+    }, []);
+
     return (
         <div>
             <h1>Alura Cases - Página de perguntas FAQ</h1>
             <Link href="/">Ir para a Home</Link>
+            <ul>
+                {faq.map(({ answer, question}) => (
+                    <li key={question}>
+                        <article>
+                            <h2>{question}</h2>
+                            <p>{answer}</p>
+                        </article>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
