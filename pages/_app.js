aff86fa52e0caf1721b5230abddf08ce215c2f84
ARTHUR DE MATOS ELSNER <arthurdev93@gmail.com>
2023-02-07 07:47:50 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import Head from "next/head";
 import GlobalStyle from "../src/theme/GlobalStyle";
+import ReactDOM from 'react-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faCheckSquare, faCoffee)
+
 
 function MyApp ({ Component, pageProps }) {
     return (
